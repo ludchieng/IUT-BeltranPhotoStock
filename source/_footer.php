8c9d762ad1a84708cb657bed9b80a0cfbfc614da
Ludwig Chieng <lud.chieng@hotmail.fr>
2019-03-07 20:43:49 +0100

    Added header footer search

---
+++ source/_footer.php
@@ -0,0 +1,62 @@
+<footer>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-3">
+        <h1>Assistance</h1>
+        <div>
+          Posez votre question :<br/>
+          contact@beltraphotostock.com
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <h1>Services</h1>
+        <ul>
+          <li><a href="#">Contactez-nous</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Commande et paiement</a></li>
+          <li><a href="#">Livraison</a></li>
+          <li><a href="#">Retour</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-4">
+        <h1>Explorer</h1>
+        <table>
+          <tr>
+            <td><a href="">Science</a></td>
+            <td><a href="">Sport</a></td>
+            <td><a href="">Urbain</a></td>
+            <td><a href="">Nourriture</a></td>
+          </tr>
+          <tr>
+            <td><a href="">Technologie</a></td>
+            <td><a href="">Culture</a></td>
+            <td><a href="">Paysage</a></td>
+            <td><a href="">Art</a></td>
+          </tr>
+          <tr>
+            <td><a href="">Architecture</a></td>
+            <td><a href="">Nature</a></td>
+            <td><a href="">Industrie</a></td>
+            <td><a href="">Voyage</a></td>
+          </tr>
+          <tr>
+            <td><a href="">Abstrait</a></td>
+            <td><a href="">Musique</a></td>
+            <td><a href="">Lifestyle</a></td>
+            <td><a href="">Animaux</a></td>
+          </tr>
+        </table>
+      </div>
+      <div class="col-lg-2">
+        <h1>Suivez nous</h1>
+        <ul class="inline">
+          <li><a href=""><img src="./public/assets/icon-instagram.svg"></a></li>
+          <li><a href=""><img src="./public/assets/icon-twitter.svg"></a></li>
+          <li><a href=""><img src="./public/assets/icon-facebook.svg"></a></li>
+        </ul>
+        <h1>Administration</h1>
+        <a href="#">Connexion</a>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ source/_header.php
@@ -0,0 +1,23 @@
+<header class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div id="logo" class="navbar-header">
+      <a class="navbar-brand" href="#">BeltranPhotoStock</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="active"><a href="#">Explorer</a></li>
+      <li><a href="#">Thèmes</a></li>
+      <li><a href="#">Événements</a></li>
+      <li><a href="#">Collections privées</a></li>
+      <li>
+        <a href="#">
+          <img src="./public/assets/icon-panier.svg">
+        </a>
+      </li>
+      <li class="navbar-account">
+        <a href="#">
+          <span>Jean Dupont</span>
+          <img src="./public/assets/icon-client.svg">
+        </a></li>
+    </ul>
+  </div>
+</header>
